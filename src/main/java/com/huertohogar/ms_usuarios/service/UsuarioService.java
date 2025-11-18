172d9090bb87abce382dd17f5b5fc1ee8a5c35e3
dxminique <do.cofre@duocuc.cl>
2025-11-18 12:30:59 -0300

    feat(service): agregar UsuarioService con lógica CRUD

---
+++ src/main/java/com/huertohogar/ms_usuarios/service/UsuarioService.java
@@ -0,0 +1,45 @@
+package com.huertohogar.ms_usuarios.service;
+
+import com.huertohogar.ms_usuarios.model.Usuario;
+import com.huertohogar.ms_usuarios.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public List<Usuario> getAll() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario getById(Long id) {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado con id: " + id));
+    }
+    public Usuario create(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario update(Long id, Usuario datos) {
+        Usuario existente = getById(id);
+        existente.setNombre(datos.getNombre());
+        existente.setEmail(datos.getEmail());
+        existente.setPassword(datos.getPassword());
+        existente.setRol(datos.getRol());
+        existente.setActivo(datos.getActivo());
+        return usuarioRepository.save(existente);
+    }
+
+    public void delete(Long id) {
+        Usuario existente = getById(id);
+        usuarioRepository.delete(existente);
+    }
+
+}
