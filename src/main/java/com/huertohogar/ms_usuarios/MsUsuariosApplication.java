4c26e78230db9992511f9fc8fdb94b7c897c6cad
dxminique <do.cofre@duocuc.cl>
2025-11-18 13:45:48 -0300

    Configuración MySQL + crarreglode UsuarioRepository + ajustes de conexión

--- src/main/java/com/huertohogar/ms_usuarios/MsUsuariosApplication.java
+++ src/main/java/com/huertohogar/ms_usuarios/MsUsuariosApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MsUsuariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsUsuariosApplication.class, args);
 	}
-
 }
